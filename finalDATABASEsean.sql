de291ad13dea6ff00bbf0e4e282c6f49a26c3eaa
PizzaSpark <seanpizzaspark@gmail.com>
2023-11-28 00:27:06 +0800

    updated sql file

--- finalDATABASEsean.sql
+++ finalDATABASEsean.sql
@@ -31,7 +31,7 @@ CREATE TABLE `tbl_admin` (
   PRIMARY KEY (`admin_id`),
   KEY `fk_tbl_admin_tbl_user_idx` (`user_id`),
   CONSTRAINT `fk_tbl_admin_tbl_user` FOREIGN KEY (`user_id`) REFERENCES `tbl_user` (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -40,7 +40,7 @@ CREATE TABLE `tbl_admin` (
 
 LOCK TABLES `tbl_admin` WRITE;
 /*!40000 ALTER TABLE `tbl_admin` DISABLE KEYS */;
-INSERT INTO `tbl_admin` VALUES (9,6,'EKOOOO');
+INSERT INTO `tbl_admin` VALUES (9,6,'Dr Eko'),(10,12,'adminSean');
 /*!40000 ALTER TABLE `tbl_admin` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -59,7 +59,7 @@ CREATE TABLE `tbl_audit` (
   PRIMARY KEY (`audit_id`),
   KEY `fk_tbl_audit_tbl_user1_idx` (`audit_user_id`),
   CONSTRAINT `fk_tbl_audit_tbl_user1` FOREIGN KEY (`audit_user_id`) REFERENCES `tbl_user` (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=106 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -68,7 +68,7 @@ CREATE TABLE `tbl_audit` (
 
 LOCK TABLES `tbl_audit` WRITE;
 /*!40000 ALTER TABLE `tbl_audit` DISABLE KEYS */;
-INSERT INTO `tbl_audit` VALUES (1,5,'logged in','20/11/2023 4:11:54 pm'),(2,5,'logged out','20/11/2023 4:12:10 pm');
+INSERT INTO `tbl_audit` VALUES (101,12,'logged in','27/11/2023 8:31:45 pm'),(102,12,'logged in','27/11/2023 8:37:56 pm'),(103,12,'logged in','27/11/2023 8:43:26 pm'),(104,12,'logged in','27/11/2023 8:50:58 pm'),(105,12,'logged in','27/11/2023 9:15:41 pm');
 /*!40000 ALTER TABLE `tbl_audit` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -94,7 +94,7 @@ CREATE TABLE `tbl_billing` (
   CONSTRAINT `fk_tbl_billing_tbl_doctor1` FOREIGN KEY (`doctor_id`) REFERENCES `tbl_doctor` (`doctor_id`),
   CONSTRAINT `fk_tbl_billing_tbl_medical_record1` FOREIGN KEY (`record_id`) REFERENCES `tbl_medical_record` (`record_id`),
   CONSTRAINT `fk_tbl_billing_tbl_patient1` FOREIGN KEY (`patient_id`) REFERENCES `tbl_patient` (`patient_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -103,6 +103,7 @@ CREATE TABLE `tbl_billing` (
 
 LOCK TABLES `tbl_billing` WRITE;
 /*!40000 ALTER TABLE `tbl_billing` DISABLE KEYS */;
+INSERT INTO `tbl_billing` VALUES (1,1,5,2,'Sunday, 26 November 2023','500','PAID'),(2,3,5,3,'Monday, 27 November 2023','1000','PAID');
 /*!40000 ALTER TABLE `tbl_billing` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -120,7 +121,7 @@ CREATE TABLE `tbl_doctor` (
   PRIMARY KEY (`doctor_id`),
   KEY `fk_tbl_doctor_tbl_user1_idx` (`user_id`),
   CONSTRAINT `fk_tbl_doctor_tbl_user1` FOREIGN KEY (`user_id`) REFERENCES `tbl_user` (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -129,7 +130,7 @@ CREATE TABLE `tbl_doctor` (
 
 LOCK TABLES `tbl_doctor` WRITE;
 /*!40000 ALTER TABLE `tbl_doctor` DISABLE KEYS */;
-INSERT INTO `tbl_doctor` VALUES (5,7,'Maddie');
+INSERT INTO `tbl_doctor` VALUES (5,7,'Maddieee'),(6,10,'Gracias');
 /*!40000 ALTER TABLE `tbl_doctor` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -153,7 +154,7 @@ CREATE TABLE `tbl_medical_record` (
   KEY `fk_tbl_medical_record_tbl_doctor1_idx` (`doctor_id`),
   CONSTRAINT `fk_tbl_medical_record_tbl_doctor1` FOREIGN KEY (`doctor_id`) REFERENCES `tbl_doctor` (`doctor_id`),
   CONSTRAINT `fk_tbl_medical_record_tbl_patient1` FOREIGN KEY (`patient_id`) REFERENCES `tbl_patient` (`patient_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -162,6 +163,7 @@ CREATE TABLE `tbl_medical_record` (
 
 LOCK TABLES `tbl_medical_record` WRITE;
 /*!40000 ALTER TABLE `tbl_medical_record` DISABLE KEYS */;
+INSERT INTO `tbl_medical_record` VALUES (2,1,5,'Sunday, 26 November 2023','aaaa','ccccc','bbbbbb'),(3,3,5,'Monday, 27 November 2023','uwuinator','yes','3x water');
 /*!40000 ALTER TABLE `tbl_medical_record` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -179,7 +181,7 @@ CREATE TABLE `tbl_patient` (
   PRIMARY KEY (`patient_id`),
   KEY `fk_tbl_patient_tbl_user1_idx` (`user_id`),
   CONSTRAINT `fk_tbl_patient_tbl_user1` FOREIGN KEY (`user_id`) REFERENCES `tbl_user` (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -188,7 +190,7 @@ CREATE TABLE `tbl_patient` (
 
 LOCK TABLES `tbl_patient` WRITE;
 /*!40000 ALTER TABLE `tbl_patient` DISABLE KEYS */;
-INSERT INTO `tbl_patient` VALUES (1,8,NULL);
+INSERT INTO `tbl_patient` VALUES (1,8,'please'),(2,9,''),(3,11,NULL);
 /*!40000 ALTER TABLE `tbl_patient` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -206,7 +208,7 @@ CREATE TABLE `tbl_user` (
   `user_isactive` int DEFAULT '0',
   `user_role` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -215,7 +217,7 @@ CREATE TABLE `tbl_user` (
 
 LOCK TABLES `tbl_user` WRITE;
 /*!40000 ALTER TABLE `tbl_user` DISABLE KEYS */;
-INSERT INTO `tbl_user` VALUES (5,'admin','471e6604ad6b4f9b85a81305feefb4f7',1,'Admin'),(6,'eko','48f6653b9e739014ddc7782df9a2d7c8',1,'Admin'),(7,'donna','83e926426234a579128e08b488e8e0c3',1,'Doctor'),(8,'shane','c02a4c2e497494764881b390fd6dd55f',1,'Patient');
+INSERT INTO `tbl_user` VALUES (5,'adminn','471e6604ad6b4f9b85a81305feefb4f7',2,'Admin'),(6,'ekoako','48f6653b9e739014ddc7782df9a2d7c8',1,'Admin'),(7,'donna','83e926426234a579128e08b488e8e0c3',1,'Doctor'),(8,'shane','c02a4c2e497494764881b390fd6dd55f',1,'Patient'),(9,'kerty','4f8bf4cbef18c2e7bb6f3d36d0c60455',1,'Patient'),(10,'doctor','6ce4be7607d13b0bf3119d47b62404c1',1,'Doctor'),(11,'patient','f71b03b9710c39949ed6770bf601fa83',1,'Patient'),(12,'admin','3225cb195fc2b6c5fd8d1fce48994e6c',1,'Admin');
 /*!40000 ALTER TABLE `tbl_user` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -226,6 +228,28 @@ UNLOCK TABLES;
 --
 -- Dumping routines for database 'db_hospital'
 --
+/*!50003 DROP PROCEDURE IF EXISTS `GetDataFromUserId` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `GetDataFromUserId`(IN tableName VARCHAR(255), IN primaryKeyColumnName VARCHAR(255), IN primaryKeyId INT)
+BEGIN
+    SET @sql_query = CONCAT('SELECT * FROM ', tableName, ' WHERE ', primaryKeyColumnName, ' = ', primaryKeyId);
+    PREPARE stmt FROM @sql_query;
+    EXECUTE stmt;
+    DEALLOCATE PREPARE stmt;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -236,4 +260,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-11-20 16:13:13
+-- Dump completed on 2023-11-28  0:25:42
